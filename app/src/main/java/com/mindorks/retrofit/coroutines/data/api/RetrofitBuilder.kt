04be7f4294f971734d33a221d117243a774de751
OpsiteLV <lucasveliabtssio@gmail.com>
2023-05-14 20:35:38 +0400

    Fin

--- app/src/main/java/com/mindorks/retrofit/coroutines/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/mindorks/retrofit/coroutines/data/api/RetrofitBuilder.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
 
-    private const val BASE_URL = "http://192.168.136.101/api/"
+    private const val BASE_URL = "http://192.168.56.1/api/"
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
